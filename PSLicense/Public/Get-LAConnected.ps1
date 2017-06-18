6d4eb3d3edfd23db21b577b1776d7afa7c868dd4
kevinblumenfeld <github@thenext.net>
2017-06-18 13:38:10 -0400

    Added help and removed one switch from Get-LAConnected

--- PSLicense/Public/Get-LAConnected.ps1
+++ PSLicense/Public/Get-LAConnected.ps1
@@ -1,13 +1,92 @@
 function Get-LAConnected {
     <#
-.Synopsis
-   Short description
+
+.SYNOPSIS
+   Connects to Office 365 services and/or Azure
+
 .DESCRIPTION
-   Long description
+   Connects to Office 365 services and/or Azure.
+
+   Connects to some or all of the Office 365/Azure services based on switches provided at runtime.
+
+   Office 365 tenant name, for example, either contoso or contoso.onmicrosoft.com must be provided with -Tenant parameter
+   For AzureOnly, provide a unique name for the -Tenant parameter
+
+   Locally saves and encrypts to a file the username and password.
+   The encrypted file...
+      
+   1. can only be used by on the computer and on the user's profile from which it was created.
+   2. is the same .txt for all the Office 365 Services.
+   3. for Azure is seperate and is a .json file.
+   
+   If Azure or Azureonly switch is used for first time:
+   
+   1. User will login as normal when prompted by Azure
+   2. User will be prompted to select which Azure Subscription
+   3. Select the subscription and click "OK"
+
+   If Azure or Azureonly switch is used:
+
+   1. User will be prompted to pick username used previously
+   2. If a new username is to be used (e.g. username not found when prompted), click Cancel to be prompted to login.
+   3. User will be prompted to select which Azure Subscription
+   4. Select the subscription and click "OK"
+
+   Directories used/created during the execution of this script 
+   
+   1. C:\ps
+   2. C:\ps\creds
+   3. C:\ps\<tenantspecified>
+
+   All saved credentials are saved in C:\ps\creds
+   Transcipt is started and kept in C:\ps\<tenantspecified>
+   
 .EXAMPLE
-   Example of how to use this cmdlet
+   Get-LAConnected -Tenant Contoso -AzureOnly
+
+   Connects to Azure Only
+   For the mandatory parameter, Tenant, simply provide something that uniquely identifies the Azure Tenant
+    
 .EXAMPLE
-   Another example of how to use this cmdlet
+   Get-LAConnected -Tenant Contoso
+
+   Connects by default to MS Online Service (MSOL) and Exchange Online (unless AzureOnly switch is used)
+   Office 365 tenant name, for example, either contoso or contoso.onmicrosoft.com must be provided with -Tenant parameter
+
+.EXAMPLE
+   Get-LAConnected -Tenant Contoso -Skype
+
+   Connects by default to MS Online Service (MSOL) and Exchange Online
+   Connects to Skype Online
+
+.EXAMPLE
+   Get-LAConnected -Tenant Contoso -SharePoint
+
+   Connects by default to MS Online Service (MSOL) and Exchange Online
+   Connects to SharePoint Online
+
+.EXAMPLE
+   Get-LAConnected -Tenant Contoso -Compliance
+
+   Connects by default to MS Online Service (MSOL) and Exchange Online
+   Connects to Compliance & Security Center
+
+.EXAMPLE
+   Get-LAConnected -Tenant Contoso -All365
+
+   Connects to MS Online Service (MSOL), Exchange Online, Skype, SharePoint & Compliance
+   
+.EXAMPLE
+   Get-LAConnected -Tenant Contoso -All365 -Azure
+
+   Connects to Azure, MS Online Service (MSOL), Exchange Online, Skype, SharePoint & Compliance
+   
+.EXAMPLE
+   Get-LAConnected -Tenant Contoso -Skype -Azure
+
+   Connects to Azure, MS Online Service (MSOL), Exchange Online & Skype
+   This is to illustrate that any number of individual services can be used to connect.
+      
 #>
     [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
     Param
@@ -23,10 +102,7 @@ function Get-LAConnected {
         [switch] $Azure,    
 
         [Parameter(Mandatory = $false)]
-        [switch] $AzureOnly,        
-                 
-        [Parameter(Mandatory = $false)]
-        [switch] $AzureAnd365,        
+        [switch] $AzureOnly,    
  
         [parameter(Mandatory = $false)]
         [switch] $Skype,
@@ -78,7 +154,7 @@ function Get-LAConnected {
                 $Credential = New-Object System.Management.Automation.PSCredential -ArgumentList $UsernameString, $PwdSecureString 
             }
             else {
-                $Credential = Get-Credential -Message "Enter a user name and password"
+                $Credential = Get-Credential -Message "Enter a username and password"
                 $Credential.Password | ConvertFrom-SecureString | Out-File "$($KeyPath)\$Tenant.cred" -Force
                 $Credential.UserName | Out-File "$($KeyPath)\$Tenant.ucred"
             }
@@ -119,16 +195,16 @@ function Get-LAConnected {
         }
 
         # Azure
-        if ($AzureAnd365 -or $AzureOnly -or $Azure) {
+        if ($AzureOnly -or $Azure) {
             $json = Get-ChildItem -Recurse -Include '*@*.json' -Path 'C:\ps\creds'
             if ($json) {
                 Write-Host   "*********************************************************************************" -foregroundcolor "magenta" -backgroundcolor "yellow"
                 Write-Host   "*********************************************************************************" -foregroundcolor "magenta" -backgroundcolor "yellow"
-                Write-Output "Select the Azure User Name and Click `"OK`" in lower right-hand corner"
-                Write-Output "Otherwise, if this is the first time using this Azure User Name click `"Cancel`""
+                Write-Output "Select the Azure username and Click `"OK`" in lower right-hand corner"
+                Write-Output "Otherwise, if this is the first time using this Azure username click `"Cancel`""
                 Write-Host   "*********************************************************************************" -foregroundcolor "magenta" -backgroundcolor "yellow"
                 Write-Host   "*********************************************************************************" -foregroundcolor "magenta" -backgroundcolor "yellow"
-                $json = $json | select name | Out-GridView -PassThru
+                $json = $json | select name | Out-GridView -PassThru -Title "Select Azure username or click Cancel to use another"
             }
             if (!($json)) {
                 $azlogin = Login-AzureRmAccount
@@ -143,7 +219,7 @@ function Get-LAConnected {
             Write-Output "Select Subscription and Click "OK" in lower right-hand corner"
             Write-Host   "*************************************************************" -foregroundcolor "magenta" -backgroundcolor "yellow"
             Write-Host   "*************************************************************" -foregroundcolor "magenta" -backgroundcolor "yellow"
-            $subscription = Get-AzureRmSubscription | Out-GridView -PassThru | Select id
+            $subscription = Get-AzureRmSubscription | Out-GridView -PassThru -Title "Choose Azure Subscription"| Select id
             Select-AzureRmSubscription -SubscriptionId $subscription.id
         }
 
